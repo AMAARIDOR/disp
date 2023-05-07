9d3f691b0461b0a568ad7341426a54291a6c3c93
Amaari <arish2k3@gmail.com>
2023-05-07 09:49:45 +0530

    add string.h library

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 #include <errno.h>
 
 // Main function
@@ -6,7 +7,8 @@ int main(int argc, char *argv[]) {
     if (argc < 3 && argc > 1) {
         // Scenario for perfect number of arguments
         printf("More than 1 argument but less than 2\n");
-    } 
+    } else
+        return -1;
 
     return 0;
 }
