b7b5139c8b53677c956bab4c529337a2d5660eeb
Amaari <arish2k3@gmail.com>
2023-05-07 10:37:49 +0530

    read test file successfully

--- main.c
+++ main.c
@@ -4,15 +4,26 @@
 
 // Main function
 int main(int argc, char *argv[]) {
+
+    FILE* fp = fopen("test.txt", "r");
+    char ch;
+
     if (argc < 3 && argc > 1) {
-        printf("The value of the argument is: %s\n", argv[1]);
+        do {
+            ch = fgetc(fp);
+            printf("%c", ch);
+        } while (ch != EOF);
+
+        return 0;
     } else if (argc <= 1) {
         // Error condition
         fprintf(stderr, "ERORR: Need at least 1 argument.\n");
+
         return 1;
     } else if (argc >= 3) {
         // Error condition
         fprintf(stderr, "ERORR: Too many arguments.\n");
+
         return 1;
     }
 
