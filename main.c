418d5701c15b9ed05c63f35976dfec102ed50d69
Amaari <arish2k3@gmail.com>
2023-05-07 10:05:11 +0530

    add errors for arguments

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
@@ -7,8 +8,15 @@ int main(int argc, char *argv[]) {
     if (argc < 3 && argc > 1) {
         // Scenario for perfect number of arguments
         printf("More than 1 argument but less than 2\n");
-    } else
-        return -1;
+    } else if (argc <= 1) {
+        // Error condition
+        fprintf(stderr, "ERORR: Need at least 1 argument.\n");
+        return 1;
+    } else if (argc >= 3) {
+        // Error condition
+        fprintf(stderr, "ERORR: Too many arguments.\n");
+        return 1;
+    }
 
     return 0;
 }
